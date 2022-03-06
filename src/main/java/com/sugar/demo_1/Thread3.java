f5dd48070241da5e8c85bc99ffc76c6d9b74c84b
肖念昕 <sugar_tech@outlook.com>
2022-03-06 23:30:59 +0800

    nil

---
+++ src/main/java/com/sugar/demo_1/Thread3.java
@@ -0,0 +1,25 @@
+package com.sugar.demo_1;
+
+/**
+ * 实现 Runnable 接口, 重写 run 方法, 丢入线程, 启动线程
+ * @author sugar
+ * @date 2022/3/6 23:14
+ */
+public class Thread3 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("子线程" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread3 thread3 = new Thread3();
+        new Thread(thread3).start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("主线程" + i);
+        }
+
+    }
+}
---
+++ src/main/java/com/sugar/demo_1/Thread4.java
@@ -0,0 +1,31 @@
+package com.sugar.demo_1;
+
+/**
+ * 多线程操作同一对象
+ * @author sugar
+ * @date 2022/3/6 23:24
+ */
+public class Thread4 implements Runnable {
+    private Integer ticket = 10;
+    @Override
+    public void run() {
+        while (true) {
+            if (ticket <= 0) {
+                break;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "-" + "抢到" + ticket--);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread4 ticket = new Thread4();
+        new Thread(ticket,"李大炮").start();
+        new Thread(ticket,"王小鸡").start();
+        new Thread(ticket,"云小米").start();
+    }
+}
